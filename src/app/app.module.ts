da421cec88552bd39bc2637fbb062a32dd4269f6
Chindi-Nagabhushan <chindinagabhushan1250@gmail.com>
2021-07-04 14:41:20 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FootComponent } from './foot/foot.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FootComponent,
+    ProductsComponent,
+    ProductInfoComponent
   ],
   imports: [
     BrowserModule,
